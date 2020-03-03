6b41f931ea02fd4022d47cf3136175948f1b135a
Igor Barreto <igorbarreto1993@hotmail.com>
2020-03-03 01:20:29 -0300

    listando os providers

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
-
 import './database';
 
 class App {
@@ -12,6 +12,10 @@ class App {
 
   middlwares() {
     this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
